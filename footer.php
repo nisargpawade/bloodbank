05ec2d69c772f08e57acb734264de1d1eb849320
nisargpawade <70917826+nisargpawade@users.noreply.github.com>
2021-11-23 00:21:23 +0530

    updated footer

--- footer.php
+++ footer.php
@@ -1,7 +1,3 @@
 <footer class="footer text-center bg-light">
-	© Copyright <span id="demo"></span> <span class="brand"> PES Blood Bank. </span> All Rights Reserved.
-	<script>
-		date = new Date();
-    	document.getElementById("demo").innerHTML=date.getFullYear();
-    </script>
+	<span id="demo"></span> <span class="brand"> PES Blood Bank </span>
 </footer>
